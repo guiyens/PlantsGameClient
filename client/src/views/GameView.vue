c8c65e7fdf837d19e04840cf22eb558b08c232a4
Guillermo Ramos Vega <guiyens@gmail.com>
2023-05-29 16:46:35 +0200

    add project

---
+++ client/src/views/GameView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="game">
+    <h1>This is a Game page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import io from "socket.io-client";
+
+export default defineComponent({
+  name: "GameView",
+});
+
+const socket = io("http://localhost:3000");
+
+socket.on("connect", () => {
+  console.log("Connected!");
+  //socket.emit("dealDeck", socket.id);
+});
+</script>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">Home</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "HomeView",
+});
+</script>
