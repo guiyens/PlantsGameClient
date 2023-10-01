1f695e6b3b93b173d5cc97db0d6f211b88c04623
Guillermo Ramos Vega <guiyens@gmail.com>
2023-10-01 14:20:26 +0200

    deploy blocked

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,6 @@ import { createPinia } from 'pinia'
 import App from '@/App.vue'
 import router from './router'
 
-window.addEventListener('beforeunload', (event) => {
-  event.preventDefault()
-  event.returnValue = 'Si sales abandonaras la partida'
-  alert('hello')
-})
-
 const app = createApp(App)
 
 app.use(createPinia())
