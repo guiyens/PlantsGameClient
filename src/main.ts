6dd446cb877e16f9514cb3c5ff3e2249bb0bf2b0
Guillermo Ramos Vega <guiyens@gmail.com>
2023-10-01 13:34:47 +0200

    try to double check reload tag

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { createPinia } from 'pinia'
 import App from '@/App.vue'
 import router from './router'
 
+window.addEventListener('beforeunload', function (event) {
+  event.returnValue = 'Write something'
+  console.log('asdad')
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
