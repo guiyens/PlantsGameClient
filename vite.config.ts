526ab185149e25b3c5cb8928140ef18ab7004c85
Guillermo Ramos Vega <guiyens@gmail.com>
2023-11-12 18:48:13 +0100

    server

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8000
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
